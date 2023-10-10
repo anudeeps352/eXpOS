df54486154e6b1166c358ba53629a070277110cc
Anudeep <anudeeps352@gmail.com>
2023-10-10 14:15:25 +0530

    multiple stages till 18

--- xfs-interface/stage16.bat
+++ xfs-interface/stage16.bat
@@ -1,15 +1,15 @@
-load --os $HOME/myexpos/spl/spl_progs/stage13_ostartup.xsm
+load --init $HOME/myexpos/expl/samples/gcd.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/int10.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/int7_stage15.xsm
 load --exhandler $HOME/myexpos/spl/spl_progs/haltprog.xsm
+load --int=timer $HOME/myexpos/spl/spl_progs/timer_stage14.xsm
+load --os $HOME/myexpos/spl/spl_progs/stage13_ostartup.xsm
 load --library $HOME/myexpos/expl/library.lib
 load --idle $HOME/myexpos/expl/samples/idle.xsm
-load --init $HOME/myexpos/expl/samples/gcd.xsm
-load --module 7 $HOME/myexpos/spl/spl_progs/bootmodule_16.xsm
 load --int=console $HOME/myexpos/spl/spl_progs/consoleint.xsm
-load --int=timer $HOME/myexpos/spl/spl_progs/timer_stage14.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/int7_stage15.xsm
 load --int=6 $HOME/myexpos/spl/spl_progs/int6.xsm
+load --module 7 $HOME/myexpos/spl/spl_progs/bootmodule_16.xsm
 load --module 5 $HOME/myexpos/spl/spl_progs/mod_5.xsm
 load --module 0 $HOME/myexpos/spl/spl_progs/mod_0.xsm
 load --module 4 $HOME/myexpos/spl/spl_progs/mod_4.xsm
