768c3af02337658c7c5106f821d7092fe2fa82fa
Anudeep <anudeeps352@gmail.com>
2023-10-11 22:46:22 +0530

    completed stage 19

--- xfs-interface/stage14_assg2.sh
+++ xfs-interface/stage14_assg2.sh
@@ -3,7 +3,7 @@ load --int=7 $HOME/myexpos/spl/spl_progs/sample_int7.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/int10.xsm
 load --init $HOME/myexpos/expl/samples/odd_14.xsm
 load --exec $HOME/myexpos/expl/samples/even_14.xsm
-load --exec $HOME/myexpos/expl/samples/primes.xsm
+load --exec $Home/myexpos/expl/samples/primes.xsm
 load --idle $HOME/myexpos/expl/samples/idle.xsm
 load --int=timer $HOME/myexpos/spl/spl_progs/timer_stage14.xsm
 load --module 7 $HOME/myexpos/spl/spl_progs/bootmodule_14_asg2.xsm
