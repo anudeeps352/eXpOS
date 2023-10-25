eb9407ff2f843de7dd714abc83171209b07995fc
Anudeep <anudeeps352@gmail.com>
2023-10-26 00:14:25 +0530

    done stage 22 assignment 3 has bugs tho

---
+++ xfs-interface/stage22_assg1.sh
@@ -0,0 +1,29 @@
+load --os $HOME/myexpos/spl/spl_progs/stage13_ostartup.xsm
+load --int=10 $HOME/myexpos/spl/spl_progs/int10.xsm
+load --int=7 $HOME/myexpos/spl/spl_progs/int7_stage15.xsm
+load --int=9 $HOME/myexpos/spl/spl_progs/int9st19.xsm
+load --int=6 $HOME/myexpos/spl/spl_progs/int6.xsm
+load --int=8 $HOME/myexpos/spl/spl_progs/int8.xsm
+load --int=11 $HOME/myexpos/spl/spl_progs/int11.xsm
+load --int=15 $HOME/myexpos/spl/spl_progs/int15.xsm
+load --int=13 $HOME/myexpos/spl/spl_progs/int13.xsm
+load --int=14 $HOME/myexpos/spl/spl_progs/int14.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --idle $HOME/myexpos/expl/samples/idle.xsm
+load --init $HOME/myexpos/expl/samples/shell.xsm
+load --exec $HOME/myexpos/expl/samples/odd_14.xsm
+load --exec $HOME/myexpos/expl/samples/even_14.xsm
+load --exec $HOME/myexpos/expl/samples/linked.xsm
+load --exec $HOME/myexpos/expl/samples/stage22.xsm
+load --module 7 $HOME/myexpos/spl/spl_progs/bootmodule_22.xsm
+load --int=console $HOME/myexpos/spl/spl_progs/consoleint.xsm
+load --int=timer $HOME/myexpos/spl/spl_progs/timer.xsm
+load --module 5 $HOME/myexpos/spl/spl_progs/mod_5.xsm
+load --module 2 $HOME/myexpos/spl/spl_progs/mod_2.xsm
+load --module 1 $HOME/myexpos/spl/spl_progs/mod_1.xsm
+load --module 0 $HOME/myexpos/spl/spl_progs/mod_0.xsm
+load --module 4 $HOME/myexpos/spl/spl_progs/mod_4.xsm
+load --int=disk $HOME/myexpos/spl/spl_progs/diskint.xsm
+load --exhandler $HOME/myexpos/spl/spl_progs/exhandler.xsm
+exit
+
