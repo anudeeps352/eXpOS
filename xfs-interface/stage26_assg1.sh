108ef5b51345f6fb59bd5762398034fdd6b1b188
Anudeep <anudeeps352@gmail.com>
2023-11-07 20:54:43 +0530

    done stages 25 and 26 with assignments and bug fixes

---
+++ xfs-interface/stage26_assg1.sh
@@ -0,0 +1,44 @@
+load --os $HOME/myexpos/spl/spl_progs/stage26_ostartup.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --idle $HOME/myexpos/expl/samples/idle.xsm
+load --init $HOME/myexpos/expl/samples/login.xsm
+load --shell $HOME/myexpos/expl/samples/newshell.xsm
+load --exhandler $HOME/myexpos/spl/spl_progs/exhandler.xsm
+load --int=4 $HOME/myexpos/spl/spl_progs/int4.xsm
+load --int=5 $HOME/myexpos/spl/spl_progs/int5.xsm
+load --int=6 $HOME/myexpos/spl/spl_progs/int6.xsm
+load --int=7 $HOME/myexpos/spl/spl_progs/int7.xsm
+load --int=8 $HOME/myexpos/spl/spl_progs/int8.xsm
+load --int=9 $HOME/myexpos/spl/spl_progs/int9st19.xsm
+load --int=10 $HOME/myexpos/spl/spl_progs/int10.xsm
+load --int=11 $HOME/myexpos/spl/spl_progs/int11.xsm
+load --int=12 $HOME/myexpos/spl/spl_progs/int12check.xsm
+load --int=13 $HOME/myexpos/spl/spl_progs/int_13a.xsm
+load --int=14 $HOME/myexpos/spl/spl_progs/int_14a.xsm
+load --int=15 $HOME/myexpos/spl/spl_progs/int15.xsm
+load --int=16 $HOME/myexpos/spl/spl_progs/int16.xsm
+load --int=17 $HOME/myexpos/spl/spl_progs/int17.xsm
+load --int=console $HOME/myexpos/spl/spl_progs/consoleint.xsm
+load --int=timer $HOME/myexpos/spl/spl_progs/timer.xsm
+load --int=disk $HOME/myexpos/spl/spl_progs/diskint.xsm
+load --module 0 $HOME/myexpos/spl/spl_progs/mod_0.xsm
+load --module 1 $HOME/myexpos/spl/spl_progs/mod_1.xsm
+load --module 2 $HOME/myexpos/spl/spl_progs/mod_2.xsm
+load --module 3 $HOME/myexpos/spl/spl_progs/mod_3.xsm
+load --module 4 $HOME/myexpos/spl/spl_progs/mod_4.xsm
+load --module 5 $HOME/myexpos/spl/spl_progs/mod_5.xsm
+load --module 7 $HOME/myexpos/spl/spl_progs/bootmodule_26.xsm
+load --exec $HOME/myexpos/expl/samples/odd_14.xsm
+load --exec $HOME/myexpos/expl/samples/create.xsm
+load --exec $HOME/myexpos/expl/samples/delete.xsm
+load --exec $HOME/myexpos/expl/samples/read.xsm
+load --data $HOME/myexpos/expl/samples/sample.dat
+load --data $HOME/myexpos/expl/samples/numbers.dat
+load --exec $HOME/myexpos/expl/samples/even_14.xsm
+load --exec $HOME/myexpos/expl/samples/linked.xsm
+load --exec $HOME/myexpos/expl/samples/mergesq.xsm
+load --exec $HOME/myexpos/expl/samples/mergecn.xsm
+load --exec $HOME/myexpos/expl/samples/lu.xsm
+load --exec $HOME/myexpos/expl/samples/ru.xsm
+exit
+
